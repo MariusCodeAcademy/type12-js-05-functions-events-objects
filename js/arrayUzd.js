6cf6be113813025f32babd69f85be161b7a2cc03
Marius <marius@example.com>
2022-07-11 13:47:43 +0300

    uzd

--- js/arrayUzd.js
+++ js/arrayUzd.js
@@ -1,2 +1,9 @@
 const pavadinimai = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake'];
 const pagalbiniai = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' '];
+
+// atspausdinti is masyvu
+
+// 1. my sister is in the lake
+// 2. hammer is on the table
+// 3. sun is in the sky
+// 4. apple is with my sister
