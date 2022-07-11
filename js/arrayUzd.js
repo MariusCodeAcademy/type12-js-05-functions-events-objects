14e8ab2ee009a12b66d7b0b951cf6e490fc46188
Marius <marius@example.com>
2022-07-11 13:56:33 +0300

    my sister is

--- js/arrayUzd.js
+++ js/arrayUzd.js
@@ -1,9 +1,11 @@
-const pavadinimai = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake'];
-const pagalbiniai = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' '];
+const pav = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake'];
+const pglb = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' '];
 
 // atspausdinti is masyvu
 
 // 1. my sister is in the lake
+const lake = pglb[3] + pglb[7] + pav[1] + pglb[7] + pglb[4];
+console.log('lake ===', lake);
 // 2. hammer is on the table
 // 3. sun is in the sky
 // 4. apple is with my sister
