d883ab0d567991934f6d88cf3e97f4fd5b6eb7c0
Marius <marius@example.com>
2022-07-11 16:00:41 +0300

    fn

--- js/obj.js
+++ js/obj.js
@@ -7,6 +7,9 @@ const user1 = {
   age: 26,
   town: 'Vilnius',
   email: 'Serbentautas@gmail.com',
+  greet: function () {
+    console.log('Hello world');
+  },
 };
 
 const savybe = 'email';
@@ -18,3 +21,9 @@ user1.age = 101;
 console.log('user1.age ===', user1.age);
 
 console.log('user1[savybe] ===', user1[savybe]);
+
+user1['firstName'];
+user1.firstName;
+
+user1.country = 'Lietuva'; // prideti reiksme i objekta
+console.log('user1.country ===', user1.country);
